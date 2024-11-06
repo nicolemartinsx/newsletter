12ce0efb404aa9ab338542874a958b416ff0d9e6
Nicole <nicolemartins@alunos.utfpr.edu.br>
2024-11-05 21:35:59 -0300

    feat(cliente): criação da tela de login

---
+++ components/login-form.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function LoginForm() {
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Digite seu email e senha para acessar a sua conta
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">E-mail</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="email@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Senha</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline">
+                Esqueceu a senha?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Não possui uma conta?{" "}
+          <Link href="#" className="underline">
+            Criar conta
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+const config: Config = {
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				'sans': ['var(--font-geist-sans)', ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)'
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
+};
+export default config;
